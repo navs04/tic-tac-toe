eb94c857da3210e8a180a24cbdded5c925779d5b
Navya Bhatia <navyabhatia48@gmail.com>
2026-01-11 01:31:56 +0530

    game logic

--- script.js
+++ script.js
@@ -1,22 +1,63 @@
+let matrix= [
+    ["","",""],
+    ["","",""],
+    ["","",""]
+];
+
 const board = document.querySelector("#gameboard");
 
 function gameboard(){
-    let matrix = [];
+    let isClicked = false;
 
     for(let i=0;i<3;i++){
-        matrix[i] = [];
-
         for(let j=0;j<3;j++){
             const ele = document.createElement('div');
             ele.classList.add('element')
 
-            matrix[i][j] = ele;
-
+            ele.textContent = matrix[i][j];
             board.appendChild(ele);
+
+            ele.addEventListener('click', () => {
+                ele.textContent = 'X';
+                matrix[i][j] = 'X';
+                isClicked = true;
+            })
         }
     }
-
-    console.log(matrix);
 }
 
 gameboard();
+
+var score=0;
+
+function game(){
+    if(matrix[0][0]==matrix[0][1] && matrix[0][1]==matrix[0][2] && matrix[0][0]!=""){
+        score++;
+    }
+    if(matrix[1][0]==matrix[1][1] && matrix[1][1]==matrix[1][2] && matrix[1][0]!=""){
+        score++;
+    }
+    if(matrix[2][0]==matrix[2][1] && matrix[2][1]==matrix[2][2] && matrix[2][0]!=""){
+        score++;
+    }
+    if(matrix[0][0]==matrix[1][0] && matrix[1][0]==matrix[2][0] && matrix[0][0]!=""){
+        score++;
+    }
+    if(matrix[0][1]==matrix[1][1] && matrix[1][1]==matrix[2][1] && matrix[0][1]!=""){
+        score++;
+    }
+    if(matrix[0][2]==matrix[1][2] && matrix[1][2]==matrix[2][2] && matrix[0][2]!=""){
+        score++;
+    }
+    if(matrix[0][0]==matrix[1][1] && matrix[1][1]==matrix[2][2] && matrix[0][0]!=""){
+        score++;
+    }
+    if(matrix[0][2]==matrix[1][1] && matrix[1][1]==matrix[2][0] && matrix[0][2]!=""){
+        score++;
+    }
+}
+
+console.log(matrix);
+
+game();
+console.log(score);
