6c2fc258e31fcd137e9d553dea67d071ae56e9ec
Navya Bhatia <navyabhatia48@gmail.com>
2026-01-13 01:37:37 +0530

    refactor: separate game state, logic and UI into modules

--- script.js
+++ script.js
@@ -1,97 +1,156 @@
-let matrix= [
-    ["","",""],
-    ["","",""],
-    ["","",""]
-];
+const gameBoard = (function(){
+    let matrix= [
+        ["","",""],
+        ["","",""],
+        ["","",""]
+    ];
 
-const board = document.querySelector("#gameboard");
-let gameOver = false;
-let move = 0;
+    const getBoard = () => {
+        return matrix;
+    }
+
+    const setCell = (row, col, value) => {
+        if(matrix[row][col] == ""){
+            matrix[row][col] = value;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    const resetBoard = () => {
+        matrix= [
+            ["","",""],
+            ["","",""],
+            ["","",""]
+        ];
+    }
+
+    return {getBoard, setCell, resetBoard};
+})();
 
 const result = document.querySelector("#result");
 const winner = document.createElement('p');
 result.appendChild(winner);
 
-function gameboard(){
+const gameController = (function(){
     let currentPlayer ="X";
+    let move = 0;
+    let win = false;
+    let gameOver = false;
 
-    for(let i=0;i<3;i++){
-        for(let j=0;j<3;j++){
-            const ele = document.createElement('div');
-            ele.classList.add('element')
+    const playMove = (row, col) => {
+        if(gameOver){
+            return false;
+        }
 
-            ele.textContent = matrix[i][j];
-            board.appendChild(ele);
-
-            ele.addEventListener('click', () => {
-                if(matrix[i][j] == ""){
-                    if(gameOver == false){
-                        move++;
-                        matrix[i][j] = currentPlayer;
-                        ele.textContent = currentPlayer;
-
-                        game();
-
-                        if(win == true){
-                            gameOver = true;
-                            winner.textContent = `${currentPlayer} won !`;
-                        }
-                        else if(move == 9){
-                            gameOver = true;
-                            winner.textContent = `It's  a tie !`;
-                        }
-
-                        currentPlayer = currentPlayer === "X" ? "O" : "X";
-                    }
-                }
-            })
+        const result = gameBoard.setCell(row, col, currentPlayer);
+        if(result == false){
+            return false;
+        }
+        else{
+            move++;
+            return true;
         }
     }
-}
 
-gameboard();
+    const checkWin = () => {
+        const board = gameBoard.getBoard();
 
-let win = false;
+        if(board[0][0]==board[0][1] && board[0][1]==board[0][2] && board[0][0]!=""){
+            win = true;
+        }
+        else if(board[1][0]==board[1][1] && board[1][1]==board[1][2] && board[1][0]!=""){
+            win = true;
+        }
+        else if(board[2][0]==board[2][1] && board[2][1]==board[2][2] && board[2][0]!=""){
+            win = true;
+        }
+        else if(board[0][0]==board[1][0] && board[1][0]==board[2][0] && board[0][0]!=""){
+            win = true;
+        }
+        else if(board[0][1]==board[1][1] && board[1][1]==board[2][1] && board[0][1]!=""){
+            win = true;
+        }
+        else if(board[0][2]==board[1][2] && board[1][2]==board[2][2] && board[0][2]!=""){
+            win = true;
+        }
+        else if(board[0][0]==board[1][1] && board[1][1]==board[2][2] && board[0][0]!=""){
+            win = true;
+        }
+        else if(board[0][2]==board[1][1] && board[1][1]==board[2][0] && board[0][2]!=""){
+            win = true;
+        }
 
-function game(){
-    if(matrix[0][0]==matrix[0][1] && matrix[0][1]==matrix[0][2] && matrix[0][0]!=""){
-        win = true;
+        if(win == true){
+            gameOver = true;
+            return currentPlayer;
+        }
+        else if(move == 9){
+            gameOver = true;
+            return "tie";
+        }
+        else{
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
+        }
     }
-    else if(matrix[1][0]==matrix[1][1] && matrix[1][1]==matrix[1][2] && matrix[1][0]!=""){
-        win = true;
-    }
-    else if(matrix[2][0]==matrix[2][1] && matrix[2][1]==matrix[2][2] && matrix[2][0]!=""){
-        win = true;
-    }
-    else if(matrix[0][0]==matrix[1][0] && matrix[1][0]==matrix[2][0] && matrix[0][0]!=""){
-        win = true;
-    }
-    else if(matrix[0][1]==matrix[1][1] && matrix[1][1]==matrix[2][1] && matrix[0][1]!=""){
-        win = true;
-    }
-    else if(matrix[0][2]==matrix[1][2] && matrix[1][2]==matrix[2][2] && matrix[0][2]!=""){
-        win = true;
-    }
-    else if(matrix[0][0]==matrix[1][1] && matrix[1][1]==matrix[2][2] && matrix[0][0]!=""){
-        win = true;
-    }
-    else if(matrix[0][2]==matrix[1][1] && matrix[1][1]==matrix[2][0] && matrix[0][2]!=""){
-        win = true;
-    }
-}
 
+    const resetGame = () => {
+        currentPlayer = "X";
+        gameOver = false;
+        win = false;
+        move = 0;
+        winner.textContent = "";
+    }
+
+    return {playMove, checkWin, resetGame};
+})();
+
+const gameboard = document.querySelector("#gameboard");
 const resetBtn = document.querySelector("#reset");
-resetBtn.addEventListener('click', () => {
-    board.innerHTML = "";
-    matrix= [
-        ["","",""],
-        ["","",""],
-        ["","",""]
-    ];
-    gameboard();
 
-    gameOver = false;
-    win = false;
-    move = 0;
-    winner.textContent = "";
-})
+const displayController = (function(){
+    const render = () => {
+        gameboard.innerHTML = "";
+        const board = gameBoard.getBoard();
+
+        for(let i=0;i<3;i++){
+            for(let j=0;j<3;j++){
+                const ele = document.createElement('div');
+                ele.classList.add('element')
+
+                ele.textContent = board[i][j]; 
+                gameboard.appendChild(ele);
+
+                ele.addEventListener('click', () => {
+                    const moved = gameController.playMove(i, j);
+                    if(moved){
+                        const result = gameController.checkWin();
+
+                        render();
+
+                        if(result == "X" || result == "O"){
+                            winner.textContent = `${result} won !`;
+                        }
+                        else if(result == "tie"){
+                            winner.textContent = `It's a tie !`;
+                        }
+                    }
+                })
+
+            }
+        }
+    }
+
+    resetBtn.addEventListener('click', () => {
+        gameboard.innerHTML = "";
+        gameBoard.resetBoard();
+        gameController.resetGame();
+        render();
+    })
+
+    return {render};
+})();
+
+displayController.render();
